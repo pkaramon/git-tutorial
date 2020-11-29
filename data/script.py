53d59913d6c7e166740760fe879a23d1dc4b3cc0
Piotr Karamon <piotrkaramon3@gmail.com>
2020-11-29 17:43:14 +0100

    added Person class to script.py

--- data/script.py
+++ data/script.py
@@ -10,6 +10,28 @@ def is_odd(x: int):
         return x % 2 == 1
 
 
+class Person:
+    def __init__(self, name: str, age: int, married: bool):
+        self._age = age
+        self._name = name
+        self.married = married
+
+    @property
+    def name(self):
+        return self._name.capitalize()
+
+    @property
+    def age(self):
+        return self._age
+
+    @age.setter
+    def age(self, value):
+        if type(value) is int and value >= self._age:
+            self._age = value
+        else:
+            raise ValueError("invalid age: {value}")
+
+
 def file_reader(filename: Path):
     if filename.exists():
         readout = filename.open("rt").read()
