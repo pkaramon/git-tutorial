07d693fff334a07bbcf06c0574fd885533d00138
Piotr Karamon <piotrkaramon3@gmail.com>
2020-11-29 12:09:35 +0100

    added imporant functions to script.py

--- data/script.py
+++ data/script.py
@@ -1,4 +1,18 @@
+from pathlib import Path
+
 print("hello world!!!")
 
+
 def is_odd(x: int):
-    return x % 2 == 1
+    if type(x) is not int:
+        raise ValueError("wrong type")
+    else:
+        return x % 2 == 1
+
+
+def file_reader(filename: Path):
+    if filename.exists():
+        readout = filename.open("rt").read()
+        return readout
+    else:
+        raise ValueError("file: {filename} does not exist")
