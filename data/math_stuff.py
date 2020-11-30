b49d68d8f29b81c5169cb80c8dc79408759b649c
Piotr Karamon <piotrkaramon3@gmail.com>
2020-11-30 22:28:35 +0100

    Merged add_trig_functions with main

--- data/math_stuff.py
+++ data/math_stuff.py
@@ -1,3 +1,6 @@
+import math
+
+
 def add(x, y):
     return x + y
 
@@ -16,3 +19,11 @@ def mul_vec(u, v):
     if len(u) != len(v):
         raise ValueError("lengths of vectors dont match")
     return [x * y for x, y in zip(u, v)]
+
+
+def arcsin(angle):
+    return math.asin(angle)
+
+
+def arccos(angle):
+    return math.acos(angle)
