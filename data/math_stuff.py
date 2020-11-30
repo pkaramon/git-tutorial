34e7467f1b85824e491d6fd6c72999fcd18f5d66
Piotr Karamon <piotrkaramon3@gmail.com>
2020-11-30 22:54:57 +0100

    added func_vec function

--- data/math_stuff.py
+++ data/math_stuff.py
@@ -21,6 +21,12 @@ def mul_vec(u, v):
     return [x * y for x, y in zip(u, v)]
 
 
+def func_vec(u, v, func):
+    if len(u) != len(v):
+        raise ValueError("lengths of vectors dont match")
+    return [func(x, y) for x, y in zip(u, v)]
+
+
 def arcsin(angle):
     return math.asin(angle)
 
